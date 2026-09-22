2f03c4f5c00afdaee62cd999d56d8be05c450aa7
Kopihue <258140777+Kopihue@users.noreply.github.com>
2026-09-22 00:14:13 -0300

    Rewrite after-dark

---
+++ lua/kopischemes/after-dark/lua/after-dark/highlights.lua
@@ -0,0 +1,80 @@
+local M = {}
+
+M.apply = function (opts)
+    local palette = require("after-dark.palette")
+
+    local transparency = opts.transparency and "none" or palette.base00
+    local bold = opts.bold or false
+    local italic = opts.italic or false
+
+    local highlights = {
+	["Normal"]		= { bg = transparency },
+	["Statement"]		= { fg = palette.base14, bold = bold },
+	["@variable"]		= { fg = palette.base10 },
+	["Function"]		= { fg = palette.base12 },
+	["Special"]		= { fg = palette.base12 },
+	["Constant"]		= { fg = palette.base09 },
+	["String"]		= { fg = palette.base11, italic = italic },
+	["Delimiter"]		= { fg = palette.base03 },
+	["Operator"]		= { fg = palette.base14 },
+	["Identifier"]		= { fg = palette.base08 },
+	["CursorLine"]		= { bg = palette.base01 },
+	["CursorLineNr"]	= { fg = palette.base12 },
+	["Visual"]		= { bg = palette.base01 },
+	["Search"]		= { bg = palette.base02 },
+	["CurSearch"]		= { bg = palette.base02 },
+	["Comment"]		= { fg = palette.base16 },
+	["Type"]   	 	= { fg = palette.base15 },
+	["WinSeparator"] 	= { fg = palette.base01 },
+	["LineNr"]       	= { fg = palette.base02 },
+
+	-- python
+	["@module.python"]			= { fg = palette.base10 },
+	["@function.builtin.python"]		= { fg = palette.base12 },
+	["@constructor.python"]			= { fg = palette.base12 },
+	["@variable.builtin.python"]		= { fg = palette.base13 },
+
+	-- ibl
+	["IblIndent"] = { fg = palette.base02 },
+	["IblScope"]  = { fg = palette.base02 },
+
+	-- lsp
+	["DiagnosticSignError"]        = { fg = palette.base15 },
+	["DiagnosticVirtualTextError"] = { fg = palette.base15 },
+	["DiagnosticSignWarn"] 	       = { fg = palette.base12 },
+	["DiagnosticVirtualTextWarn"]  = { fg = palette.base12 },
+	["DiagnosticSignHint"] 	       = { fg = palette.base16 },
+	["DiagnosticVirtualTextHint"]  = { fg = palette.base16 },
+	["DiagnosticSignInfo"] 	       = { fg = palette.base13 },
+	["DiagnosticVirtualTextInfo"]  = { fg = palette.base13 },
+
+	-- blink.cmp
+	["Pmenu"] 		    = { bg = palette.base01 },
+	["BlinkCmpMenuSelection"]   = { bg = palette.base02 },
+	["BlinkCmpKind"] 	    = { fg = palette.base14 },
+	["BlinkCmpKindFunction"]    = { fg = palette.base12 },
+	["BlinkCmpKindSnippet"]     = { fg = palette.base11 },
+
+	-- Telescope
+	["TelescopeSelection"]      = { bg = palette.base02 },
+	["TelescopeResultsBorder"]  = { fg = palette.base02 },
+	["TelescopeResultsTitle"]   = { fg = palette.base12 },
+	["TelescopePromptBorder"]   = { fg = palette.base02 },
+	["TelescopePromptTitle"]    = { fg = palette.base12 },
+	["TelescopePromptNormal"]   = { fg = palette.base12 },
+	["TelescopeSelectionCaret"] = { fg = palette.base12 },
+	["TelescopeResultsCount"]   = { bg = palette.base12 },
+	["TelescopePreviewBorder"]  = { fg = palette.base02 },
+	["TelescopePreviewTitle"]   = { fg = palette.base12 },
+
+	-- C
+	["@type.builtin.c"]		= { fg = palette.base15 },
+	["@string.escape.c"]		= { fg = palette.base13 },
+    }
+
+    for field, value in pairs(highlights) do
+	vim.api.nvim_set_hl(0, field, value)
+    end
+end
+
+return M
--- lua/kopischemes/after-dark/lua/after-dark/init.lua
+++ lua/kopischemes/after-dark/lua/after-dark/init.lua
@@ -1,94 +1,19 @@
-local palette = require("after-dark.palette")
-
-local highlights = function(opts)
-    local background = opts.transparency and "none" or palette.background
-    local cursor_line = opts.transparency and "none" or palette.cursor_line
-    local bold = opts.bold or false
-    local italic = opts.italic or false
-    local cursor_line_number = opts.colored_line_number and palette.func or palette.foreground
-
-    local highlights = {
-	["Normal"] = { bg = background },
-	["Statement"] = { fg = palette.keyword },
-	["@variable"] = { fg = palette.type },
-	["Function"] = { fg = palette.func, bold = bold },
-	["Special"] = { fg = palette.func },
-	["String"] = { fg = palette.string, italic = italic },
-	["Operator"] = { fg = palette.keyword },
-	["Comment"] = { fg = palette.comment, italic = italic },
-	["Constant"] = { fg = palette.number },
-	["Delimiter"] = { fg = palette.line_number },
-	["Statusline"] = { bg = background },
-	["StatuslineNC"] = { bg = background },
-	["Type"] = { fg = palette.variable, bold = bold },
-	["PreProc"] = { fg = palette.cursor },
-	["CursorLine"] = { bg = cursor_line },
-	["Visual"] = { bg = palette.cursor_line },
-	["CursorLineNr"] = { fg = cursor_line_number },
-	["CurSearch"] = { bg = palette.cursor_line },
-	["Search"] = { bg = palette.cursor_line },
-	["Identifier"] = { fg = palette.keyword },
-
-	-- treesitter
-	["@module.rust"] = { fg = palette.crate },
-	["@property.toml"] = { fg = palette.func },
-	["@markup.heading.1.markdown"] = { fg = palette.func },
-	["@markup.heading.2.markdown"] = { fg = palette.crate },
-	["@markup.heading.3.markdown"] = { fg = palette.type },
-	["@markup.heading.4.markdown"] = { fg = palette.keyword },
-	["@markup.list.markdown"] = { fg = palette.keyword },
-	["@type.builtin.c"] = { fg = palette.crate },
-
-	-- blink
-	["Pmenu"] = { bg = background },
-	["BlinkCmpMenuSelection"] = { bg = palette.cursor_line},
-	["BlinkCmpKind"] = { fg = palette.func },
-	["BlinkCmpKindFunction"] = { fg = palette.keyword },
-	["BlinkCmpKindSnippet"] = { fg = palette.number },
-
-	-- Telescope
-	["TelescopeSelection"] = { bg = palette.cursor_line },
-	["TelescopeResultsBorder"] = { fg = palette.cursor_line },
-	["TelescopeResultsTitle"] = { fg = palette.func },
-	["TelescopePromptBorder"] = { fg = palette.cursor_line },
-	["TelescopePromptTitle"] = { fg = palette.func },
-	["TelescopePromptNormal"] = { fg = palette.crate },
-	["TelescopeSelectionCaret"] = { fg = palette.func },
-	["TelescopeResultsCount"] = { bg = palette.crate },
-	["TelescopePreviewTitle"] = { fg = palette.func },
-	["TelescopePreviewBorder"] = { fg = palette.cursor_line },
-
-	-- HTML, CSS, JS
-	["@tag.delimiter.html"] = { fg = palette.line_number },
-	["@tag.attribute.html"] = { fg = palette.type },
-	["Title"] = { fg = palette.crate },
-
-	-- lsp
-	["DiagnosticSignError"] = { fg = palette.crate },
-	["DiagnosticSignWarn"] = { fg = palette.keyword },
-	["DiagnosticSignHint"] = { fg = palette.func },
-	["DiagnosticSignInfo"] = { fg = palette.number },
-
-	-- C
-	["@type.builtin.c"] = { fg = palette.crate, italic = italic },
-	["@keyword.import.c"] = { fg = palette.keyword, italic = italic, },
-
-	-- python
-	["@variable.builtin.python"] = { fg = palette.crate, italic = italic },
-
-	-- netrw
-	["netrwPlain"] = { fg = palette.crate },
-	["netrwDir"] = { fg = palette.keyword },
-	["netrwExe"] = { fg = palette.func },
-    }
-    return highlights
-end
-
+local highlights = require("after-dark.highlights")
 local M = {}
 
+M.opts = {
+    transparency = false,
+    italic = true,
+    bold = true,
+}
+
 M.setup = function(opts)
-    for field, value in pairs(highlights(opts)) do
-	vim.api.nvim_set_hl(0, field, value)
+    if opts then
+	for key, value in pairs(opts) do
+	    M.opts[key] = value
+	end
+
+	highlights.apply(M.opts)
     end
 end
 
